318332722ffe1521b847f25fe06444918d553a4b
Cleomir <cleomir.mg@hotmail.com>
2021-04-02 15:01:48 +0300

    Customize theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 
 import { Create } from "./pages/Create";
 import { Notes } from "./pages/Notes";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Notes} />
-        <Route exact path="/create" component={Create} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Notes} />
+          <Route exact path="/create" component={Create} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
