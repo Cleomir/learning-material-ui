30aa871b80edeaeac95b58b58577a776bfc7ff27
Cleomir <cleomir.mg@hotmail.com>
2021-04-05 14:23:31 +0300

    List json server data

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export const Notes = () => {
-  return <div>Notes page</div>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/notes")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
+  return (
+    <div>
+      {notes.map((note: { id: number; title: string }) => (
+        <p key={note.id}>{note.title}</p>
+      ))}
+    </div>
+  );
 };
