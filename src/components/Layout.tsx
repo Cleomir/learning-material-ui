eea7dfbe0899cf01545ec1fd29a15f89c872b8f1
Cleomir <cleomir.mg@hotmail.com>
2021-04-09 12:21:44 +0300

    Add permanent drawer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Drawer, Typography } from "@material-ui/core";
 
+const drawerWidth = 240;
 const useStyles = makeStyles({
   page: {
     backgroundColor: "#f9f9f9",
     width: "100%",
   },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
+  },
 });
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">Ninja Notes</Typography>
+        </div>
+      </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
   );
