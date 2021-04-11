29d05d7b2332321518be31181876128c8ae58978
Cleomir <cleomir.mg@hotmail.com>
2021-04-11 13:40:50 +0300

    Add conditional styles

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -4,9 +4,20 @@ import {
   CardContent,
   IconButton,
   Typography,
+  makeStyles,
 } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
 import { INote } from "../types/INote";
+
+const useStyles = makeStyles({
+  test: {
+    border: (note: INote) => {
+      if (note.category === "work") {
+        return "1px solid red";
+      }
+    },
+  },
+});
 
 export const NoteCard = ({
   note,
@@ -15,9 +26,11 @@ export const NoteCard = ({
   note: INote;
   handleDelete: (id: string) => Promise<void>;
 }) => {
+  const classes = useStyles(note);
+
   return (
     <div>
-      <Card elevation={3}>
+      <Card elevation={3} className={classes.test}>
         <CardHeader
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
